20f769b96c8b174cc1dee27abab85627d9141f7c
Austine904 <ochiengaustine89@gmail.com>
2025-02-21 23:57:35 +0300

    fet db data

--- welcome.php
+++ welcome.php
@@ -43,7 +43,8 @@
                     values(?, ?, ?, ?)");
                     $stmt->bind_param("ssii",$fname, $email, $phone, $date);
                      if ($stmt->execute()) {
-                        echo "Registration Successful";
+                        echo "Registration Successful"; 
+
                     } else {
                         echo "Error: " . $stmt->error;
                     }
@@ -57,46 +58,93 @@
         }
     ?>
             
-
-    
     <form class="" action="#" method="post">
         
-    <div>
-        <label for="fname">Full Names:</label>
-        <input type="text" id="fname" name="fname" placeholder="Oriagi Oriagi Oriagi">
-    </div>
-   
+        <div>
+            <label for="fname">Full Names:</label>
+            <input type="text" id="fname" name="fname" placeholder="Oriagi Oriagi Oriagi">
+        </div>
+    
 
+        <br>
+
+
+        <div>
+            <label for="email">Your Email:</label>
+            <input type="email" id="email" name="email" placeholder="nyamweya@fmail.com">
+        </div>
+
+        <br>
+
+
+        <div>
+            <label for="phone">Phone Number:</label>
+            <input type="tel" id="phone" name="phone" placeholder="254 769 696 969">
+        </div>
+
+        <br>
+
+        <div>
+            <label for="date">Date:</label>
+            <input type="date" id="date" name="date">
+        </div>
+
+        <br>
+
+
+        <div>
+            <input type="submit">
+        </div> 
+    </form>
+
+    <h1>List Of Bash Goers</h1>
     <br>
+    <Table>
+        <thread>
+            <tr>
+            <th>ID</th>
+            <th>fname</th>
+            <th>email</th>
+            <th>phone</th>
+            <th>Date</th>
+            </tr>
+        </thread>
 
+        <tbody>
+            <?php
+             $db_server = "localhost";
+             $db_user ="root";
+             $db_pass = "";
+             $db_name = "oriagidb";
+             $conn = "";
 
-    <div>
-        <label for="email">Your Email:</label>
-        <input type="email" id="email" name="email" placeholder="nyamweya@fmail.com">
-    </div>
+             $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
-    <br>
+             if (!$conn) {
+                die("Connection failed: " . $conn->mysqli_connect_error);
+             }
+            
+             if($conn){
 
+                //read db data
+                $sql = "SELECT * FROM bashdb";
+                $result = $conn->query($sql);
 
-    <div>
-        <label for="phone">Phone Number:</label>
-        <input type="tel" id="phone" name="phone" placeholder="254 769 696 969">
-    </div>
+                if (!$result){
+                    die("invalid querry: " . $conn->error);
+                }
 
-    <br>
-
-    <div>
-        <label for="date">Date:</label>
-        <input type="date" id="date" name="date">
-    </div>
-
-    <br>
-
-
-    <div>
-        <input type="submit">
-    </div> 
- 
-</form>
+                // read data of each row
+                while($row = $result->fetch_assoc()){
+                    echo "<tr>
+                    <td>" . $row["id"] . "</td>
+                    <td>" . $row["fname"] . "</td>
+                    <td>" . $row["email"] . "</td>
+                    <td>" . $row["phone"] . "</td>
+                    <td>" . $row["date"] . "</td>
+                   </tr>";
+                }
+        </tbody>
+    </Table>
 </body>
 </html>
